6b42e5f8cc78b5fc7631000e07c5045e7935e873
MathieuMarthy <mathieu.marthy@gmail.com>
2024-01-25 00:43:50 +0100

    Square model | logic class

---
+++ jeu-de-la-vie/src/logic/GameOfLife.cs
@@ -0,0 +1,30 @@
+﻿using jeu_de_la_vie.models;
+
+namespace jeu_de_la_vie.logic;
+
+public class GameOfLife {
+    public Square[,] Matrix {
+        get;
+        private set;
+    }
+    private readonly int _len;
+    private readonly Random _random = new ();
+
+    public GameOfLife(int len) {
+        this._len = len;
+        this.Matrix = new Square[len, len];
+    }
+
+    private Square GetRandomSquare() {
+        int randomValue = this._random.Next(0, 2);
+        return new Square(randomValue);
+    }
+
+    public void RandomBoard() {
+        for (int i = 0; i < this._len; i++) {
+            for (int j = 0; j < this._len; j++) {
+                this.Matrix[i, j] = this.GetRandomSquare();
+            }
+        }
+    }
+}
---
+++ jeu-de-la-vie/src/models/Square.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+
+namespace jeu_de_la_vie.models;
+
+public class Square {
+    private int _state;
+    
+    public Square(int state) {
+        this._state = state;
+    }
+    
+    public bool IsAlive() {
+        return this._state == 1;
+    }
+    
+    public Color GetColor() {
+        return this._state == 1 ? Raylib.WHITE : Raylib.BLACK;
+    }
+}
