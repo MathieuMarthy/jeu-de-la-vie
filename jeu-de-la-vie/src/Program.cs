369e25a773b20b307916c39e98e1a06dbf9980ce
MathieuMarthy <mathieu.marthy@gmail.com>
2024-01-25 00:43:20 +0100

    src dir

--- jeu-de-la-vie/src/Program.cs
+++ jeu-de-la-vie/src/Program.cs
@@ -1,4 +1,6 @@
-﻿using Raylib_CsLo;
+﻿using jeu_de_la_vie.logic;
+using jeu_de_la_vie.render;
+using Raylib_CsLo;
 
 public static class Program {
     
@@ -11,6 +13,10 @@ public static class Program {
         Raylib.InitWindow(WIDTH, HEIGHT, "Jeu de la vie");
         Raylib.SetTargetFPS(60);
         
+        // init game of life
+        var gameOfLife = new GameOfLife(30);
+        gameOfLife.RandomBoard();
+        
         // Main loop
         while (!Raylib.WindowShouldClose()) {
             
@@ -18,6 +24,9 @@ public static class Program {
             Raylib.ClearBackground(Raylib.BLACK);
             
             Raylib.DrawText("Hello, world!", WIDTH / 2, HEIGHT / 2, 20, Raylib.WHITE);
+            
+            // render all squares
+            RenderMatrix.MakeRenderMatrix(gameOfLife.Matrix);
             
             Raylib.EndDrawing();
         }
